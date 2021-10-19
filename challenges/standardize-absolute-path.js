f4e2d9dd2d3041665a19b8699248a90fa340ac07
ammiel <acruz@copper.com>
2021-10-18 22:27:51 -0400

    fix logic

--- challenges/standardize-absolute-path.js
+++ challenges/standardize-absolute-path.js
@@ -4,22 +4,25 @@
  * 
  * Given an absolute pathname that may have . or .. as part of it, return the shortest standardized path.
  * For example, given "/usr/bin/../bin/./scripts/../", return "/usr/bin/".
+ * 
+ * Time Complexity: O(n)
  */
 
 function standardized(path) {
-  let dirs = path.split('/');
-
-  let standardDirs = [];
+  const stack = [];
+  const dirs = path.split(/\//);
 
   dirs.forEach(dir => {
-    if (dir === '..') {
-      if (standardDirs.length > 1) {
-        standardDirs.pop();
-      } else if (dir !== '.') {
-        standardDirs.push(dir);
-      }
-    }
+    if (dir === '..' && dirs.length > 1) {
+      stack.pop();
+    } else if (dir !== '.') {
+      stack.push(dir);
+    } 
   });
 
-  return standardDirs.join('/');
+  return stack.join('/');
 }
+
+console.log(standardized("/usr/bin/../bin/./scripts/../../../../")); // '/a/b/c'
+console.log(standardized('/a/./b/../../c/')); // '/c'
+console.log(standardized('/usr/bin/../bin/./scripts/../')); // '/usr/bin/'
